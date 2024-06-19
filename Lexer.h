b6b562312e487e3deb95cbb86c727b7a7b6c2b2f
Abderrahmane Ahachad <abderrahmane.ahachad.cs@gmail.com>
2024-06-19 18:22:46 +0100

    further tokenizations

--- Lexer.h
+++ Lexer.h
@@ -1,9 +1,10 @@
 #include <string>
 #include <list>
 #include "utilities.h"
+
 using namespace std;
 
-enum TokenType {
+enum Token {
     L_BRACE,    // {
     R_BRACE,    // }
     L_BRACKET,  // [
@@ -15,15 +16,8 @@ enum TokenType {
     STRING
 };
 
-struct Token {
-    TokenType token_type;
-    string lexeme;
-    Token(TokenType token_type, const string& lexeme): 
-            token_type(token_type), lexeme(lexeme) {}
-};
-
 class Tokenization {
-    list<Token>& token_list;
+    const list<Token>& token_list;
     list<Token>::iterator current;
 public :
     Tokenization(list<Token>& token_list): token_list(token_list){
@@ -33,30 +27,76 @@ public :
         current++;
         return *prev(current);
     }
+    bool is_valid() {
+        return token_list.size();
+    }
 };
 
 Tokenization tokenize(string& text) {
     list<Token> token_list;
-    for(int i = 0; i<text.size(); ++i) {
+    for(int i = 0; i < text.size(); ++i) {
         if(is_white_space(text[i])) {
             continue;
         }
         if(text[i] == '{') {
-            token_list.push_back(Token(L_BRACE, '{'));
+            token_list.push_back(Token(L_BRACE));
             continue;
         }
         if(text[i] == '}') {
-            token_list.push_back(Token(R_BRACE, '}'));
+            token_list.push_back(Token(R_BRACE));
             continue;
         }
         if(text[i] == '[') {
-            token_list.push_back(Token(L_BRACKET, '['));
+            token_list.push_back(Token(L_BRACKET));
             continue;
         }
         if(text[i] == ']') {
-            token_list.push_back(Token(R_BRACKET, ']'));
+            token_list.push_back(Token(R_BRACKET));
             continue;
         }
-        ...
+        if(text[i] == ',') {
+            token_list.push_back(Token(COMMA));
+            continue;
+        }
+        if(text[i] == 'n') {
+            if(text.substr(i, 4) == "null") {
+                token_list.push_back(Token(NULL_));
+                i+=3;
+                continue;
+            }
+            else {
+                return Tokenization(list<Token>()); 
+            }
+        }
+        if(text[i] == 't') {
+            if(text.substr(i, 4) == "true") {
+                token_list.push_back(Token(BOOLEAN));
+                i+=3;
+                continue;
+            }
+            else {
+                return Tokenization(list<Token>()); 
+            }
+        }
+        if(text[i] == 'f') {
+            if(text.substr(i, 4) == "false") {
+                token_list.push_back(Token(BOOLEAN));
+                i+=3;
+                continue;
+            }
+            else {
+                return Tokenization(list<Token>()); 
+            }
+        }
+        if(is_numerical(text[i])){
+            while(i<text.size() && is_numerical(text[i])) {
+                i++;
+            }
+            token_list.push_back(Token(NUMBER));
+            if(i == text.size())
+                return Tokenization(token_list);
+            i--;
+        }
+
     }
 }
