deba8d473d38eea09aa966322a9423610dfe26ad
Abderrahmane Ahachad <abderrahmane.ahachad.cs@gmail.com>
2024-06-19 17:11:49 +0100

    tokenization

--- Lexer.h
+++ Lexer.h
@@ -1,4 +1,6 @@
 #include <string>
+#include <list>
+#include "utilities.h"
 using namespace std;
 
 enum TokenType {
@@ -13,8 +15,48 @@ enum TokenType {
     STRING
 };
 
-
-class Token {
-    TokenType tocken_type;
+struct Token {
+    TokenType token_type;
     string lexeme;
+    Token(TokenType token_type, const string& lexeme): 
+            token_type(token_type), lexeme(lexeme) {}
 };
+
+class Tokenization {
+    list<Token>& token_list;
+    list<Token>::iterator current;
+public :
+    Tokenization(list<Token>& token_list): token_list(token_list){
+        current = this->token_list.begin();
+    }
+    Token next() {
+        current++;
+        return *prev(current);
+    }
+};
+
+Tokenization tokenize(string& text) {
+    list<Token> token_list;
+    for(int i = 0; i<text.size(); ++i) {
+        if(is_white_space(text[i])) {
+            continue;
+        }
+        if(text[i] == '{') {
+            token_list.push_back(Token(L_BRACE, '{'));
+            continue;
+        }
+        if(text[i] == '}') {
+            token_list.push_back(Token(R_BRACE, '}'));
+            continue;
+        }
+        if(text[i] == '[') {
+            token_list.push_back(Token(L_BRACKET, '['));
+            continue;
+        }
+        if(text[i] == ']') {
+            token_list.push_back(Token(R_BRACKET, ']'));
+            continue;
+        }
+        ...
+    }
+}
